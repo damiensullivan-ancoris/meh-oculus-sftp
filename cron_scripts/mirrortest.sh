7dbc5b5235e8c70bb6374f1774779b3d5098566d
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-02 15:15:48 +0000

    djs edits

--- cron_scripts/mirrortest.sh
+++ cron_scripts/mirrortest.sh
@@ -8,16 +8,27 @@ FTPSHOST=192.168.18.23
 
 #MEH Sftp Server Source Folder. 
 SOURCEDIR=Temp/djstest
+#SOURCEDIR=CDS_2018/SubmissionTemp
 
 #GCP GCS Bucket. 
 DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/djstest
 
+
+#-I CDSEDS63_Freeze_* -I CDSEDS63_Flex_* -I CDSEDS63_SinceFriday_* -I CDSEDS63_FiveWeeks_*
+
 lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
 set xfer:clobber on
+mirror -I *.log -I *.txt $SOURCEDIR $DESTDIR
+mirror -I *.log -I *.txt $SOURCEDIR $DESTDIR
 mirror -I *.log -I *.txt $SOURCEDIR $DESTDIR
 bye
 EOF
 
+# lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+# set xfer:clobber on
+# mirror -I CDSEDS63_Freeze_* -I CDSEDS63_Flex_* -I CDSEDS63_SinceFriday_* -I CDSEDS63_FiveWeeks_* $SOURCEDIR $DESTDIR
+# bye
+# EOF
 
 # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
 # set xfer:clobber on
