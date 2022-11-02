7651b46375f2efb14bb5df477ca9ce204ed8c953
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-02 13:33:02 +0000

    djs edits

--- cron_scripts/cds_2018_submissiontemp.sh
+++ cron_scripts/cds_2018_submissiontemp.sh
@@ -8,7 +8,7 @@ FTPSHOST=192.168.18.23
 SOURCEDIR=CDS_2018/SUSdownload
 
 #GCP GCS Bucket. 
-DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CDS_2018/SUSdownload
+DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CDS_2018/SubmissionTemp
 
 lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSEDS63_Freeze_* CDSEDS63_Flex_* CDSEDS63_SinceFriday_* ; lpwd; bye"
 
