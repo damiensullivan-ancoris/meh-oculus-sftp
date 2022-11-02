e6b66bdce8b3e53f22bdb6b32c1a13164d4aab14
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-02 15:28:38 +0000

    djs edits

--- cron_scripts/cds_2018_submissiontemp.sh
+++ cron_scripts/cds_2018_submissiontemp.sh
@@ -10,9 +10,18 @@ SOURCEDIR=CDS_2018/SubmissionTemp
 #GCP GCS Bucket. 
 DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CDS_2018/SubmissionTemp
 
-lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSEDS63_Freeze_* CDSEDS63_Flex_* CDSEDS63_SinceFriday_* CDSEDS63_FiveWeeks_* ; lpwd; bye"
-lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSOUT62_Freeze_* CDSOUT62_Flex_* CDSOUT62_SinceFriday_* CDSOUT62_FiveWeeks_*; lpwd; bye"
-lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSAPK62_Freeze_* CDSAPK62_Flex_* CDSAPK62_SinceFriday_* CDSAPK62_FiveWeeks_* CDSAPK62_HRG_*; lpwd; bye"
+lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+set xfer:clobber on
+mirror -I CDSEDS63_Freeze_* -I CDSEDS63_Flex_* -I CDSEDS63_SinceFriday_* -I CDSEDS63_FiveWeeks_* $SOURCEDIR $DESTDIR
+mirror -I CDSOUT62_Freeze_* -I CDSOUT62_Flex_* -I CDSOUT62_SinceFriday_* -I CDSOUT62_FiveWeeks_* $SOURCEDIR $DESTDIR
+mirror -I CDSAPK62_Freeze_* -I CDSAPK62_Flex_* -I CDSAPK62_SinceFriday_* -I CDSAPK62_FiveWeeks_* -I CDSAPK62_HRG_* $SOURCEDIR $DESTDIR
+bye
+EOF
+
+# ALTERNATE MGET EVERY TIME APPROACH
+# lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSEDS63_Freeze_* CDSEDS63_Flex_* CDSEDS63_SinceFriday_* CDSEDS63_FiveWeeks_* ; lpwd; bye"
+# lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSOUT62_Freeze_* CDSOUT62_Flex_* CDSOUT62_SinceFriday_* CDSOUT62_FiveWeeks_*; lpwd; bye"
+# lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget CDSAPK62_Freeze_* CDSAPK62_Flex_* CDSAPK62_SinceFriday_* CDSAPK62_FiveWeeks_* CDSAPK62_HRG_*; lpwd; bye"
 
 # NOTES:
 # mehdwssdt\c$\DataDumps\CDS_2018\SubmissionTemp\CDSEDS63_Freeze_
