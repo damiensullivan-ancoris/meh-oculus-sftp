2ff4bd2ee6839a8fd7af9ea96d590be0b781e869
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-02 13:26:05 +0000

    djs edits

--- cron_scripts/cds_2018_archive.sh
+++ cron_scripts/cds_2018_archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER=oculusagent
+USER=oculusagentro
 PASSWD=$(gcloud secrets versions access latest --secret="oculusagent-ftps-password")
 
 # MEH Sftp Server Host.
@@ -12,4 +12,9 @@ SOURCEDIR=CDS_2018/Archive
 #GCP GCS Bucket. 
 DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CDS_2018/Archive
 
+# Prefix OUTCDS, APKCDS, ECSCDS
 lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget outcds*.txt ecscds*.xml apkcds*.txt; lpwd; bye"
+
+# NOTES: Grab file names with the prefix OUTCDS, APKCDS, ECSCDS 
+# AND \z.csv,  \z.txt and \z.xml
+
