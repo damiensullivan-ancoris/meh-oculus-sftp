370904f386980417b21337a47c7b91bb5ca80dc4
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-03 13:49:01 +0000

    djs edits

--- cron_scripts/mirrortest.sh
+++ cron_scripts/mirrortest.sh
@@ -1,47 +1,51 @@
-# #!/bin/bash
+#!/bin/bash
 
-# USER=oculusagentro
-# PASSWD=$(gcloud secrets versions access latest --secret="oculusagent-ftps-password")
+USER=oculusagentro
+PASSWD=$(gcloud secrets versions access latest --secret="oculusagent-ftps-password")
 
-# # # MEH Sftp Server Host.
-# FTPSHOST=192.168.18.23
+# # MEH Sftp Server Host.
+FTPSHOST=192.168.18.23
 
-# # #MEH Sftp Server Source Folder. 
-# SOURCEDIR=Temp/djstest
-# # SOURCEDIR=CDS_2018/SubmissionTemp
+# #MEH Sftp Server Source Folder. 
+SOURCEDIR=Temp/djstest
+# SOURCEDIR=CDS_2018/SubmissionTemp
 
-# # #GCP GCS Bucket. 
-# DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/djstest
+# #GCP GCS Bucket. 
+DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/djstest
 
-# lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF 
-# set xfer:clobber on
-# mirror -r -v -I *.log -I *.txt $SOURCEDIR $DESTDIR 
-# bye
-# EOF 
+echo "--- SYNC STARTS: $(date '+%d-%m-%YT%T.%3N')" 
 
-# # # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
-# # # set xfer:clobber on
-# # # mirror -I CDSEDS63_Freeze_* -I CDSEDS63_Flex_* -I CDSEDS63_SinceFriday_* -I CDSEDS63_FiveWeeks_* $SOURCEDIR $DESTDIR
-# # # bye
-# # # EOF
+lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF 
+set xfer:clobber on
+mirror -r -v -I *.log -I *.txt $SOURCEDIR $DESTDIR 
+bye
+EOF
 
-# # # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
-# # # set xfer:clobber on
-# # # cd $SOURCEDIR
-# # # pwd
-# # # ls
-# # # lcd $DESTDIR
-# # # lpwd
-# # # !dir 
-# # # bye
-# # # EOF
+echo "--- SYNC ENDS: $(date '+%d-%m-%YT%T.%3N')" 
 
-# # # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
-# # # set xfer:clobber on
-# # # cd $SOURCEDIR
-# # # ls
-# # # lcd $DESTDIR 
-# # # mget *.* 
-# # # lpwd
-# # # bye
-# # # EOF
+# # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+# # set xfer:clobber on
+# # mirror -I CDSEDS63_Freeze_* -I CDSEDS63_Flex_* -I CDSEDS63_SinceFriday_* -I CDSEDS63_FiveWeeks_* $SOURCEDIR $DESTDIR
+# # bye
+# # EOF
+
+# # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+# # set xfer:clobber on
+# # cd $SOURCEDIR
+# # pwd
+# # ls
+# # lcd $DESTDIR
+# # lpwd
+# # !dir 
+# # bye
+# # EOF
+
+# # lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+# # set xfer:clobber on
+# # cd $SOURCEDIR
+# # ls
+# # lcd $DESTDIR 
+# # mget *.* 
+# # lpwd
+# # bye
+# # EOF
