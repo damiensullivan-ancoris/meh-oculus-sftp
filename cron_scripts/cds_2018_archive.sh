301f4edf47097a50ee4aef0cc25302c3d5f60bb6
Damien Sullivan <dsullivan@anc0844.home>
2022-11-03 08:04:58 +0000

    djs edits

--- cron_scripts/cds_2018_archive.sh
+++ cron_scripts/cds_2018_archive.sh
@@ -12,8 +12,15 @@ SOURCEDIR=CDS_2018/Archive
 #GCP GCS Bucket. 
 DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CDS_2018/Archive
 
+lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+set xfer:clobber on
+mirror -I outcds*.txt -I ecscds*.xml -I apkcds*.txt $SOURCEDIR $DESTDIR
+bye
+EOF
+
+
 # Prefix OUTCDS, APKCDS, ECSCDS
-lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget outcds*.txt ecscds*.xml apkcds*.txt; lpwd; bye"
+#lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget outcds*.txt ecscds*.xml apkcds*.txt; lpwd; bye"
 
 # NOTES: Grab file names with the prefix OUTCDS, APKCDS, ECSCDS 
 # AND \z.csv,  \z.txt and \z.xml
--- cron_scripts/cds_2018_susdownload.sh
+++ cron_scripts/cds_2018_susdownload.sh
@@ -12,7 +12,14 @@ SOURCEDIR=CDS_2018/SUSdownload
 #GCP GCS Bucket. 
 DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CDS_2018/SUSdownload
 
-lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget *.csv; lpwd; bye"
+lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+set xfer:clobber on
+mirror -I APK_SUS_Download.csv -I ECS_SUS_Download.csv -I OUT_SUS_Download.csv $SOURCEDIR $DESTDIR
+bye
+EOF
+
+# DOWNLOAD Completely every time
+#lftp -c "open ftp://$USER:$PASSWD@$FTPSHOST:21; set xfer:clobber on; cd $SOURCEDIR; ls; lcd $DESTDIR; mget *.csv; lpwd; bye"
 
 ## NOTES
 # File Manifest
