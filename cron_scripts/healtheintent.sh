5c1bec40b79ded48a5fff0ceef2cc2b0dc760880
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-03 11:38:03 +0000

    djs edits

---
+++ cron_scripts/healtheintent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER=oculusagentro
+PASSWD=$(gcloud secrets versions access latest --secret="oculusagent-ftps-password")
+
+# MEH Sftp Server Host.
+FTPSHOST=192.168.18.23
+
+#MEH Sftp Server Source Folder. 
+SOURCEDIR=HealthEintent
+
+#GCP GCS Bucket. 
+DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/HealthEintent
+
+lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+set xfer:clobber on
+mirror -r -I HCCG_UK_MOOR_COVID_INPATIENT* -I AdmCare_v1o6* -I NCLMDS_BatchSend.bat $SOURCEDIR $DESTDIR
+bye
+EOF
+
+
+
+
