89be2d72b3a38f88b6ebdc5169dc64f864ec4568
Damien Sullivan <dsullivan@ANC0844.local>
2022-11-03 11:23:27 +0000

    djs edits

---
+++ cron_scripts/casenotes_archive.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER=oculusagentro
+PASSWD=$(gcloud secrets versions access latest --secret="oculusagent-ftps-password")
+
+# MEH Sftp Server Host.
+FTPSHOST=192.168.18.23
+
+#MEH Sftp Server Source Folder. 
+SOURCEDIR=CaseNotes/Archive
+
+#GCP GCS Bucket. 
+DESTDIR=/home/oculusagent/meh-ftps-sync/meh-oculus-datadumps/CaseNotes/Archive
+
+lftp -u "$USER","$PASSWD" ftp://$FTPSHOST:21 <<EOF
+set xfer:clobber on
+mirror -r -I Manifest*.txt -I Activity*.txt Inventory*.txt $SOURCEDIR $DESTDIR
+bye
+EOF
+
+
+
+
